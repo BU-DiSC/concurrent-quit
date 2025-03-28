79b2bd1c0298fb54f3e2d61594bfb7c9c161f030
Aneesh Raman <aneeshr@bu.edu>
2025-03-28 06:10:31 +0100

    init commit - moved over v3 code from bu-disc/quick-insertion-tree

---
+++ src/include/trees.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <trees/ConcurrentQuITBTree.hpp>
+#include <trees/ConcurrentSimpleBTree.hpp>
+#include <trees/ConcurrentTailBTree.hpp>
+#include <trees/LILBTree.hpp>
+#include <trees/QuITBTree.hpp>
+#include <trees/SimpleBTree.hpp>
+#include <trees/TailBTree.hpp>
